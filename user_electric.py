1aca43fa73925f7b053aa9d0761967f2f71c5245
fanfanda <1050158648@qq.com>
2018-02-03 18:38:03 +0800

    user info and electric infos

---
+++ user_electric.py
@@ -0,0 +1,14 @@
+class user_electric:
+    """
+       用户信息以及用户用电信息
+    """
+    def __init__(self,*profiles):
+        self.ID=profiles[0]
+        self.dataDate=profiles[1]
+        self.dataType=profiles[2]
+        self.orgNO=profiles[3]
+        self.getDate=profiles[4]
+        self.dataPointFlag=profiles[5]
+        self.dataWholeFlag=profiles[6]
+        self.electric_data=profiles[7:]
+        self.normalized_electric_data=self.electric_data/sum(self.electric_data)

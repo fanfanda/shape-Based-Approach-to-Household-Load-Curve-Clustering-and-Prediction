dc2336a5e7b02cc8e90a1e1b17a124ab63097552
fanfanda <fanfanda@ict.ac.cn>
2018-02-05 20:47:37 +0800

    just for test

--- read_data_do_cluster.py
+++ read_data_do_cluster.py
@@ -3,10 +3,11 @@ import user_electric
 import numpy as np
 
 for i in range(50):
-    with open('/home/fanfanda/portData2015/part-000'+str(i), 'r') as f:                          
+    with open('/home/fanfanda/portData2015/part-000'+str(i).zfill(2), 'r') as f:                          
          data = f.readlines()  #txt中所有字符串读入data
          print(data)
          print(type(data))
+         break
          
          
 

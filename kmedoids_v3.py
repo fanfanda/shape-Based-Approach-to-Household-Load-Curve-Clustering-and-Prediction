dec09f4e67b440320b05124d1ddcbd02b7e0ea8c
fanfanda <fanfanda@ict.ac.cn>
2018-02-06 11:17:24 +0800

    nothing

--- kmedoids_v3.py
+++ kmedoids_v3.py
@@ -38,7 +38,7 @@ def fastdtw(x, y, dist):
     return D1[-1, -1] / sum(D1.shape)
 
 class k_Medoids():
-    def __init__(self,data,k=6,batch_size=1000,max_iterators=20):
+    def __init__(self,data,k=20,batch_size=1000,max_iterators=20):
         self.data=data
         self.k=k
         self.batch_size=batch_size
@@ -105,22 +105,22 @@ class k_Medoids():
                 break
         return class_assignments, ids_of_medoids
 
-## Generate Fake Data
-print("Initializing Data.")
-ds = 5
-ks = 6
-ns = ks * 10000
-#generate test data......
-data = np.random.normal(size=(ns, ds))
-for kk in range(ks):
-    dd = (kk-1)%ds
-    data[kk*ns//ks:(kk+1)*ns//ks,dd] += 3*ds*kk
-###compute dis for each pairs
-##print("compute dis for each pairs......")
-##pair_dis = pairwise_distances(x, metric=dist)
+# ## Generate Fake Data
+# print("Initializing Data.")
+# ds = 5
+# ks = 6
+# ns = ks * 10000
+# #generate test data......
+# data = np.random.normal(size=(ns, ds))
+# for kk in range(ks):
+#     dd = (kk-1)%ds
+#     data[kk*ns//ks:(kk+1)*ns//ks,dd] += 3*ds*kk
+# ###compute dis for each pairs
+# ##print("compute dis for each pairs......")
+# ##pair_dis = pairwise_distances(x, metric=dist)
 
-## doing the k-medoids clustering....
-print("doing Kmedoids.....")
-t = k_Medoids(data)
-final_assignments, final_medoid_ids = t.kmeds()
+# ## doing the k-medoids clustering....
+# print("doing Kmedoids.....")
+# t = k_Medoids(data)
+# final_assignments, final_medoid_ids = t.kmeds()
 

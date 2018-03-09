21fcf04582c75abd01071e7dced2f7c90923b790
fanfanda <fanfanda@ict.ac.cn>
2018-03-09 15:58:33 +0800

    nothing

--- read_data_do_cluster.py
+++ read_data_do_cluster.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8-*- 
 from user_electric import *
-# from k_medoids_primary import *
+import os
 from kmedoids_v3 import *
 from S_Dbw import *
-user_data=[]
-count=0
-exit_flag=False
+user_data = []
+count = 0
+exit_flag = False
+data_size = 80000
+cluster_num = 50
 #read data from database
 print("read data from database....")
 for i in range(50):
@@ -13,7 +15,7 @@ for i in range(50):
          data = f.readlines()  #txt中所有字符串读入data
          for index, item in enumerate(data):
              count+=1
-             if count==30:
+             if count==data_size:
                  exit_flag=True
                  break
              meta_data=user_electric(item.rstrip('\n').split(','))
@@ -25,19 +27,19 @@ for i in range(50):
 electric_data=np.zeros(shape=(len(user_data),24))
 for index,item in enumerate(user_data):
     electric_data[index]=item.reduce_normalized_electric_data
+if not os.path.exists("data_"+str(data_size)+".npy"):
+    print("saving data.....")
+    np.save("data_"+str(data_size)+".npy",electric_data)
+
 print("doing the cluster....")
 #doing the cluster
-cluster = k_Medoids(data=electric_data,k=3,batch_size=5)
+cluster = k_Medoids(data=electric_data,k=cluster_num,batch_size=1000)
 final_assignments, final_medoid_ids = cluster.kmeds()
 
-# file=open('cluster_result.txt','w')
-# for i in final_assignments:
-#     file.write(str(i)+'\n')
-# file.close()
-
-# file=open('cluster_medoids.txt','w')
-# file.write(str(final_medoid_ids))
-# file.close()
+if not os.path.exists("final_assignments_"+str(data_size)+"_"+str(cluster_num)+".npy"):
+    np.save("final_assignments_"+str(data_size)+"_"+str(cluster_num)+".npy",final_assignments)
+if not os.path.exists("final_medoid_ids_"+str(data_size)+"_"+str(cluster_num)+".npy"):
+    np.save("final_medoid_ids_"+str(data_size)+"_"+str(cluster_num)+".npy",final_medoid_ids)
 
 S_Dbwresult = S_Dbw(electric_data,final_assignments,final_medoid_ids)
 print(S_Dbwresult.S_Dbw_result())
